333190e6a231485eca8ecf094c254da649d6cbdb
Shane Bramble-Wade <sbramwade@msn.com>
2021-10-26 20:31:01 -0500

    lifted the state of the nav component

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,35 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {capitalizeFirstLetter} from '../../utils/helpers'
 
-
-const categories = [{
-        name: "commercial",
-        description: "Photos of grocery stores, food trucks, and other commercial projects",
-    },
-    {
-        name: "portraits",
-        description: "Portraits of people in my life"
-    },
-    {
-        name: "food",
-        description: "Delicious delicacies"
-    },
-    {
-        name: "landscape",
-        description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-    },
-];
-
-
-function categorySelected (name){
-    console.log(`${name} clicked`);
-}
 //  React Nav Component
 function Nav (){
+    const [currentCategory, setCurrentCategory] = useState(categories[0]);
+    const [categories] = useState ([
+        {
+            name: "commercial",
+            description: "Photos of grocery stores, food trucks, and other commercial projects",
+        }, {
+            name: "portraits",
+            description: "Portraits of people in my life"
+        }, {
+            name: "food",
+            description: "Delicious delicacies"
+        }, {
+            name: "landscape",
+            description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+        }
+    ]);
     return (
-        <header>
+        <header className= "flex-row px-1">
             <h2>
                 <a data-testid="link" href="/">
-                    <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+                    <span role="img" aria-label="camera"> {" "} 📸</span>{" "} Oh Snap!
                 </a>
             </h2>
             <nav>
@@ -44,12 +38,11 @@ function Nav (){
                     </li>
                     {/* .map has parenthesis rather than curly braces */}
                     {categories.map((category) => (
-                        <li 
-                        className="mx-1"
-                        key={category.name}
+                        <li className={ `mx-1 ${currentCategory.name === category.name && 'navActive'
+                        }`}key={category.name}
                         >
-                            <span onClick={()=> categorySelected(category.name)}>
-                                {category.name}
+                            <span onClick={()=> setCurrentCategory(category)}>
+                                {capitalizeFirstLetter(category.name)}
                             </span>
                         </li>
                     ))}
